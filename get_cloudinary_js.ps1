a944eef06ec55d0300852ac8a4529daac211028c
Nikita Makarov <5253450@gmail.com>
2013-07-29 01:36:29 +0400

    Improved script

--- get_cloudinary_js.ps1
+++ get_cloudinary_js.ps1
@@ -1,49 +1,122 @@
-$root = $pwd
-
-$tmp = "$root\cloudinary_temp"
-if (test-path $tmp)
-{
-  remove-item $tmp -recurse
-}
-
+write-host
 write-host "This script will download cloudinary-js files"
+write-host "Usage: .\get_cloudinary_js.ps1 [directory] [content subdirectory] [scripts subdirectory]"
+write-host "Example: .\get_cloudinary_js.ps1 c:\site content scripts"
 
-if ($args.count -ne 1)
+$tmp = "$pwd\cloudinary_temp"
+
+try
 {
-  write-host "Please, use get_cloudinary_js.ps1 directory_to_download"
+	if (test-path $tmp)
+	{
+	  remove-item $tmp -recurse
+	}
+}
+catch
+{
+  write-error "Couldn't remove temporary directory $tmp!"
+  write-host
   exit
 }
 
-if ($args.count -eq 1)
-{ $dest = "$args\Scripts" }
-else
-{ $dest = "$root\Scripts" }
+$root = $pwd
 
-write-host "Files will be downloaded to the following directory:"
-write-host $dest
-
-new-item -itemtype directory -path $tmp | out-null
-
-$shell = new-object -com shell.application
-$webclient = new-object System.Net.WebClient
-$url = "https://github.com/cloudinary/cloudinary_js/archive/master.zip"
-
-$zip = "$tmp\cloudinary_js.zip"
-$webclient.DownloadFile($url, $zip)
-
-$zipPackage = $shell.NameSpace($zip)
-$tmpDir = $shell.NameSpace($tmp)
-foreach($item in $zipPackage.items())
+if ($args.count -ge 1)
 {
-  $tmpDir.copyhere($item)
+  $root = $args[0]
 }
 
-if (-not (test-path $dest))
-{ new-item -itemtype directory -path $dest | out-null }
+if ($args.count -ge 2)
+{ $content = "$root\" + $args[1] }
+else
+{ $content = "$root\Content" }
 
-copy-item $tmp\cloudinary_js-master\js\* -destination $dest -recurse
-copy-item $tmp\cloudinary_js-master\html\* -destination $dest -recurse
+if ($args.count -ge 3)
+{ $scripts = "$root\" + $args[2] }
+else
+{ $scripts = "$root\Scripts" }
+
+write-host "Files will be downloaded to the following directory:"
+write-host "Content: $content"
+write-host "Scripts: $scripts"
+write-host "Downloading and extracting, please wait..."
+
+try
+{
+  new-item -itemtype directory -path $tmp | out-null
+}
+catch
+{
+  write-error "Couldn't create temporary directory $tmp!"
+  write-host
+  exit
+}  
+
+try
+{
+	$shell = new-object -com shell.application
+	$webclient = new-object System.Net.WebClient
+	$url = "https://github.com/cloudinary/cloudinary_js/archive/master.zip"
+
+	$zip = "$tmp\cloudinary_js.zip"
+	$webclient.DownloadFile($url, $zip)
+}
+catch
+{
+  write-error "Couldn't download file $url to $zip!"
+  write-host
+  exit
+}
+
+try
+{
+	$zipPackage = $shell.NameSpace($zip)
+	$tmpDir = $shell.NameSpace($tmp)
+	foreach($item in $zipPackage.items())
+	{
+	  $tmpDir.copyhere($item)
+	}
+}
+catch
+{
+  write-error "Couldn't extract file $zip to $tmpDir!"
+  write-host
+  exit
+}
+
+try
+{
+	if (-not (test-path $content))
+	{ new-item -itemtype directory -path $content | out-null }
+}
+catch
+{
+  write-error "Couldn't create directory $content!"
+  write-host
+  exit
+}
+
+try
+{
+	if (-not (test-path $scripts))
+	{ new-item -itemtype directory -path $scripts | out-null }
+}
+catch
+{
+  write-error "Couldn't create directory $scripts!"
+  write-host
+  exit
+}
+
+copy-item $tmp\cloudinary_js-master\js\* -destination $scripts -recurse
+copy-item $tmp\cloudinary_js-master\html\* -destination $content -recurse
 
 remove-item $tmp -recurse
 
 write-host "Downloading is completed."
+write-host
+
+get-childitem $scripts
+
+if ($scripts -ne $content)
+{ get-childitem $content }
