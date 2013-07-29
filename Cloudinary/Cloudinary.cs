c0c2a1676ef7129b11082a9b432b8d54f106d7d4
Nikita Makarov <5253450@gmail.com>
2013-07-29 16:59:01 +0400

    Added possibility to override Api object for Cloudinary object

--- Cloudinary/Cloudinary.cs
+++ Cloudinary/Cloudinary.cs
@@ -51,6 +51,7 @@ namespace CloudinaryDotNet
         public Api Api
         {
             get { return m_api; }
+            set { m_api = value; }
         }
 
         public ExplicitResult Explicit(ExplicitParams parameters)
