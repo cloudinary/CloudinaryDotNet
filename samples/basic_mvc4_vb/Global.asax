87edfd8540f66e2779540a6aadbfb090ff5ebf0b
Nikita Makarov <5253450@gmail.com>
2013-08-18 22:45:10 +0400

    Added simple vb.net example

---
+++ samples/basic_mvc4_vb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="basic_mvc4_vb.MvcApplication" Language="VB" %>
---
+++ samples/basic_mvc4_vb/Models/Image.vb
@@ -0,0 +1,9 @@
+﻿Imports CloudinaryDotNet
+
+Public Class Image
+    Public Property Caption As String
+    Public Property ShowTransform As Transformation
+    Public Property Url As String
+    Public Property Format As String
+    Public Property PublicId As String
+End Class
