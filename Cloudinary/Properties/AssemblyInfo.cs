1ccb54a10f3cab8c42c5a37f5c81b53f2645c279
Tal Lev-Ami <tal.levami@cloudinary.com>
2015-01-09 21:29:55 +0200

    Increment version 1.0.18: * sample with unsigned_upload_tag * Make api_key & api_secret optional in unsigned upload method * background_removal param in upload/update * DPR transformation parameter * Width Auto & DPR Auto * custom_coordinates in upload/update/explicit * coordinates flag in show * return_delete_token in upload * Support one-level-array in face coordinates * folder api * https sharding * use_root_path and url_suffix * signing changes (version should not be signed) * invalidate flag in bulk delete

--- Cloudinary/Properties/AssemblyInfo.cs
+++ Cloudinary/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CloudinaryDotNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Cloudinary Ltd.")]
 [assembly: AssemblyProduct("CloudinaryDotNet")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Cloudinary Ltd. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.17.0")]
+[assembly: AssemblyVersion("1.0.18.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
