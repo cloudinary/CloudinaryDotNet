23b3d7b6ad442b508a42ed4168c13bc0aa82e2de
rtlcoil <oleg@rtl.co.il>
2017-03-29 12:04:58 +0200

    AppVeyor configuration.

--- before_build.ps1
+++ before_build.ps1
@@ -1,15 +1,10 @@
 Write-Host "Copying App.config for tests"
 
-Write-Host $ApiBaseAddress
-Write-Host $CloudName
-Write-Host $ApiKey
-Write-Host $ApiSecret
-
-$strConfigSource = "d:\install\SED\app.config.sample"
-$strConfigDest = "d:\install\SED\app.config"   
+$strConfigSource = "c:\projects\cloudinary\Cloudinary.Test\app.config.sample"
+$strConfigDest = "c:\projects\cloudinary\Cloudinary.Test\app.config"   
 
 Copy-Item $strConfigSource -Destination $strConfigDest
-(Get-Content $strConfigDest).replace('ApiBaseAddress=""', 'ApiBaseAddress="' + $ApiBaseAddress + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('CloudName=""', 'CloudName="' + $CloudName + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('ApiKey=""', 'ApiKey="'+ $ApiKey + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('ApiSecret=""', 'ApiSecret="' + $ApiSecret + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('ApiBaseAddress=""', 'ApiBaseAddress="' + $env:ApiBaseAddress + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('CloudName=""', 'CloudName="' + $env:CloudName + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('ApiKey=""', 'ApiKey="'+ $env:ApiKey + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('ApiSecret=""', 'ApiSecret="' + $env:ApiSecret + '"') | Set-Content $strConfigDest
