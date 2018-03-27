901067f51807c38d6ff9dc86df5ad7d1b5d186eb
Constantine Nathanson <constantine@cloudinary.com>
2018-03-27 12:11:55 +0300

    Fix before_build.ps1

--- before_build.ps1
+++ before_build.ps1
@@ -1,10 +1,10 @@
-Write-Host "Copying App.config for tests"
+Write-Host "Copying appsettings.json for tests"
 
-$strConfigSource = "c:\projects\cloudinary\Cloudinary.Test\app.config.sample"
-$strConfigDest = "c:\projects\cloudinary\Cloudinary.Test\app.config"   
+$strConfigSource = "c:\projects\cloudinary\Shared.Tests\appsettings.json.sample"
+$strConfigDest = "c:\projects\cloudinary\Shared.Tests\appsettings.json"   
 
 Copy-Item $strConfigSource -Destination $strConfigDest
-(Get-Content $strConfigDest).replace('ApiBaseAddress=""', 'ApiBaseAddress="' + $env:ApiBaseAddress + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('CloudName=""', 'CloudName="' + $env:CloudName + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('ApiKey=""', 'ApiKey="'+ $env:ApiKey + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('ApiSecret=""', 'ApiSecret="' + $env:ApiSecret + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"ApiBaseAddress": ""', '"ApiBaseAddress": "' + $env:ApiBaseAddress + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"CloudName": ""', '"CloudName": "' + $env:CloudName + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"ApiKey": ""', '"ApiKey": "'+ $env:ApiKey + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"ApiSecret": ""', '"ApiSecret": "' + $env:ApiSecret + '"') | Set-Content $strConfigDest
