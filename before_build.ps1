8c6f13b095e457a654d6af1cc74faf512e80983a
RTLCoil <oleg@rtl.co.il>
2018-04-03 12:56:16 +0300

    Fix AppVeyor CI build

--- before_build.ps1
+++ before_build.ps1
@@ -1,10 +1,14 @@
+
 Write-Host "Copying appsettings.json for tests"
 
-$strConfigSource = "c:\projects\cloudinary\Shared.Tests\appsettings.json.sample"
-$strConfigDest = "c:\projects\cloudinary\Shared.Tests\appsettings.json"   
+$strConfigSource = "$env:APPVEYOR_BUILD_FOLDER\Shared.Tests\appsettings.json.sample"
+$strConfigDest = "$env:APPVEYOR_BUILD_FOLDER\Shared.Tests\appsettings.json"      
 
 Copy-Item $strConfigSource -Destination $strConfigDest
+
 (Get-Content $strConfigDest).replace('"ApiBaseAddress": ""', '"ApiBaseAddress": "' + $env:ApiBaseAddress + '"') | Set-Content $strConfigDest
 (Get-Content $strConfigDest).replace('"CloudName": ""', '"CloudName": "' + $env:CloudName + '"') | Set-Content $strConfigDest
 (Get-Content $strConfigDest).replace('"ApiKey": ""', '"ApiKey": "'+ $env:ApiKey + '"') | Set-Content $strConfigDest
 (Get-Content $strConfigDest).replace('"ApiSecret": ""', '"ApiSecret": "' + $env:ApiSecret + '"') | Set-Content $strConfigDest
+
+Write-Host "Done"
