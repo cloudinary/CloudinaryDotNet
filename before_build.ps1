a11f26f26baec58ccbf684038712d59fbfacf1db
Constantine Nathanson <35217733+const-cloudinary@users.noreply.github.com>
2018-04-08 17:13:25 +0300

    Fix AppVeyor CI build (#85)
    * Fix AppVeyor CI build

--- before_build.ps1
+++ before_build.ps1
@@ -1,10 +1,13 @@
-Write-Host "Copying App.config for tests"
+Write-Host "Setting up appsettings.json for tests"
 
-$strConfigSource = "c:\projects\cloudinary\Cloudinary.Test\app.config.sample"
-$strConfigDest = "c:\projects\cloudinary\Cloudinary.Test\app.config"   
+$strConfigSource = "$env:APPVEYOR_BUILD_FOLDER\Shared.Tests\appsettings.json.sample"
+$strConfigDest = "$env:APPVEYOR_BUILD_FOLDER\Shared.Tests\appsettings.json"      
 
 Copy-Item $strConfigSource -Destination $strConfigDest
-(Get-Content $strConfigDest).replace('ApiBaseAddress=""', 'ApiBaseAddress="' + $env:ApiBaseAddress + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('CloudName=""', 'CloudName="' + $env:CloudName + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('ApiKey=""', 'ApiKey="'+ $env:ApiKey + '"') | Set-Content $strConfigDest
-(Get-Content $strConfigDest).replace('ApiSecret=""', 'ApiSecret="' + $env:ApiSecret + '"') | Set-Content $strConfigDest
+
+(Get-Content $strConfigDest).replace('"ApiBaseAddress": ""', '"ApiBaseAddress": "' + $env:ApiBaseAddress + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"CloudName": ""', '"CloudName": "' + $env:CloudName + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"ApiKey": ""', '"ApiKey": "'+ $env:ApiKey + '"') | Set-Content $strConfigDest
+(Get-Content $strConfigDest).replace('"ApiSecret": ""', '"ApiSecret": "' + $env:ApiSecret + '"') | Set-Content $strConfigDest
+
+Write-Host "Done"
