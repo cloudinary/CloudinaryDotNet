3e6001674f7f33a22183c9c611638120e8d1cc1b
Constantine Nathanson <constantine@cloudinary.com>
2019-04-03 16:39:22 +0300

    Add `DeleteFolder` admin API

---
+++ Shared/Actions/DeleteFolderResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace CloudinaryDotNet.Actions
+{
+    [DataContract]
+    public class DeleteFolderResult : BaseResult
+    {
+        [DataMember(Name = "deleted")]
+        public List<string> Deleted { get; protected set; }
+    }
+}
