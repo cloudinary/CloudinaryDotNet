c81698401a5b2f3db4a0125fc97d66f7e7203e73
RTLCoil <oleg@rtl.co.il>
2020-01-17 01:57:38 +0200

    Add missing response properties to the Search API result type

--- Shared.IntegrationTests/SearchApi/SearchApiTest.cs
+++ Shared.IntegrationTests/SearchApi/SearchApiTest.cs
@@ -11,6 +11,7 @@ namespace CloudinaryDotNet.IntegrationTest.SearchApi
         private string m_searchTag;
         private string m_expressionTag;
         private string m_expressionPublicId;
+        private string m_singleResourcePublicId;
         private string[] m_publicIdsSorted;
 
         private const int INDEXING_WAIT_TIME = 5000;
@@ -18,9 +19,11 @@ namespace CloudinaryDotNet.IntegrationTest.SearchApi
         private const string SORT_FIELD = "public_id";
         private const string SORT_DIRECTION_ASC = "asc";
 
-        private const string AGG_FIELD_NAME = "aggregations";
         private const string AGG_FIELD_VALUE = "resource_type";
         private const string METADATA_FIELD_NAME = "image_metadata";
+        private const string TAGS_FIELD_NAME = "tags";
+        private const string CONTEXT_FIELD_NAME = "context";
+        private const string IMAGE_ANALYSIS_FIELD_NAME = "image_analysis";
 
         private const int FIRST_PAGE_SIZE = 1;
         private const int SECOND_PAGE_SIZE = 2;
@@ -33,15 +36,21 @@ namespace CloudinaryDotNet.IntegrationTest.SearchApi
             m_expressionTag = $"tags:{m_searchTag}";
             m_publicIdsSorted = new string[RESOURCES_COUNT];
 
+
             for (var i = 0; i < RESOURCES_COUNT; i++)
             {
-                string publicId = GetUniquePublicId();
-                var uploadParams = new ImageUploadParams()
+                var publicId = GetUniquePublicId();
+                var uploadParams = new ImageUploadParams
                 {
                     PublicId = publicId,
                     Tags = $"{m_searchTag},{m_apiTag}",
-                    File = new FileDescription(m_testImagePath)
+                    File = new FileDescription(m_testImagePath),
                 };
+                if (i == 0)
+                {
+                    m_singleResourcePublicId = publicId;
+                    uploadParams.Context = new StringDictionary { { "some key", "some value" } };
+                }
                 m_publicIdsSorted[i] = publicId;
                 m_cloudinary.Upload(uploadParams);
             }
@@ -52,26 +61,26 @@ namespace CloudinaryDotNet.IntegrationTest.SearchApi
         }
 
         [Test]
-        public void SearchApiFindResourcesByTag()
+        public void TestSearchApiFindResourcesByTag()
         {
-            SearchResult result = m_cloudinary.Search().Expression(m_expressionTag).Execute();
+            var result = m_cloudinary.Search().Expression(m_expressionTag).Execute();
 
             Assert.NotNull(result.Resources);
             Assert.AreEqual(RESOURCES_COUNT, result.Resources.Count);
         }
         
         [Test]
-        public void SearchResourceByPublicId()
+        public void TestSearchResourceByPublicId()
         {
             var result = m_cloudinary.Search().Expression(m_expressionPublicId).Execute();
-            Assert.True(result.TotalCount > 0);
+            Assert.Greater(result.TotalCount, 0);
         }
 
         [Test]
-        public void PaginateResourcesLimitedByTagAndOrderdByAscendingPublicId()
+        public void TestPaginateResourcesLimitedByTagAndOrderedByAscendingPublicId()
         {
-            SearchResult result = m_cloudinary.Search().MaxResults(FIRST_PAGE_SIZE)
-                                        .Expression(m_expressionTag).SortBy(SORT_FIELD, SORT_DIRECTION_ASC).Execute();
+            var result = m_cloudinary.Search().MaxResults(FIRST_PAGE_SIZE)
+                .Expression(m_expressionTag).SortBy(SORT_FIELD, SORT_DIRECTION_ASC).Execute();
 
             Assert.NotNull(result.Resources);
             Assert.AreEqual(FIRST_PAGE_SIZE, result.Resources.Count);
@@ -79,8 +88,8 @@ namespace CloudinaryDotNet.IntegrationTest.SearchApi
             Assert.AreEqual(m_publicIdsSorted.First(), result.Resources.First().PublicId);
 
             result = m_cloudinary.Search().MaxResults(SECOND_PAGE_SIZE)
-                                        .Expression(m_expressionTag).SortBy(SORT_FIELD, SORT_DIRECTION_ASC)
-                                        .NextCursor(result.NextCursor).Execute();
+                .Expression(m_expressionTag).SortBy(SORT_FIELD, SORT_DIRECTION_ASC)
+                .NextCursor(result.NextCursor).Execute();
 
             Assert.NotNull(result.Resources);
             Assert.AreEqual(SECOND_PAGE_SIZE, result.Resources.Count);
@@ -89,25 +98,75 @@ namespace CloudinaryDotNet.IntegrationTest.SearchApi
         }
 
         [Test]
-        public void SearchAggregateTest()
+        public void TestSearchAggregate()
         {
-            SearchResult result = m_cloudinary.Search()
-                                        .Expression(m_expressionTag).Aggregate(AGG_FIELD_VALUE).Execute();
+            var result = m_cloudinary.Search()
+                .Expression(m_expressionTag).Aggregate(AGG_FIELD_VALUE).Execute();
 
             Assert.NotNull(result.Resources);
             Assert.AreEqual(RESOURCES_COUNT, result.Resources.Count);
-            Assert.True(result.JsonObj.ToString().Contains(AGG_FIELD_NAME));
+            Assert.IsNotEmpty(result.Aggregations);
         }
 
         [Test]
-        public void SearchWithFieldTest()
+        public void TestSearchWithField()
         {
-            SearchResult result = m_cloudinary.Search().MaxResults(FIRST_PAGE_SIZE)
-                                        .Expression(m_expressionTag).WithField(METADATA_FIELD_NAME).Execute();
+            var result = m_cloudinary.Search().MaxResults(FIRST_PAGE_SIZE)
+                .Expression(m_expressionTag).WithField(METADATA_FIELD_NAME).Execute();
 
             Assert.NotNull(result.Resources);
             Assert.AreEqual(FIRST_PAGE_SIZE, result.Resources.Count);
-            Assert.True(result.JsonObj.ToString().Contains(METADATA_FIELD_NAME));
+            Assert.IsNotEmpty(result.Resources.First().ImageMetadata);
+        }
+
+        [Test]
+        public void TestRootResponseFieldsAreParsed()
+        {
+            var result = m_cloudinary.Search().MaxResults(FIRST_PAGE_SIZE)
+                .Expression(m_expressionTag).Aggregate(AGG_FIELD_VALUE).Execute();
+
+            Assert.Greater(result.TotalCount, 1);
+            Assert.Greater(result.Time, 0);
+            Assert.IsNotEmpty(result.Resources);
+            Assert.IsNotNull(result.NextCursor);
+            Assert.IsNotEmpty(result.Aggregations);
+        }
+
+        [Test]
+        public void TestResourceResponseFieldsAreParsed()
+        {
+            var result = m_cloudinary.Search().Expression($"public_id: {m_singleResourcePublicId}")
+                .WithField(METADATA_FIELD_NAME).WithField(IMAGE_ANALYSIS_FIELD_NAME)
+                .WithField(CONTEXT_FIELD_NAME).WithField(TAGS_FIELD_NAME).Execute();
+            var foundResource = result.Resources.First();
+
+            Assert.AreEqual(m_singleResourcePublicId, foundResource.PublicId);
+            Assert.AreEqual(string.Empty, foundResource.Folder);
+            Assert.AreEqual(m_singleResourcePublicId, foundResource.FileName);
+            Assert.AreEqual(FILE_FORMAT_JPG, foundResource.Format);
+            Assert.IsNotEmpty(foundResource.Version);
+            Assert.AreEqual(ResourceType.Image, foundResource.ResourceType);
+            Assert.AreEqual(STORAGE_TYPE_UPLOAD, foundResource.Type);
+            Assert.IsNotEmpty(foundResource.Created);
+            Assert.IsNotEmpty(foundResource.Uploaded);
+            Assert.AreEqual(93502, foundResource.Length);
+            Assert.IsTrue(foundResource.BackupBytes >= 0);
+            Assert.AreEqual(1920, foundResource.Width);
+            Assert.AreEqual(1200, foundResource.Height);
+            Assert.AreEqual(1.6, foundResource.AspectRatio);
+            Assert.AreEqual(2304000, foundResource.Pixels);
+            Assert.AreEqual(1, foundResource.Pages);
+            Assert.IsTrue(foundResource.Url.Contains("http://"));
+            Assert.IsTrue(foundResource.SecureUrl.Contains("https://"));
+            Assert.AreEqual("active", foundResource.Status);
+            Assert.AreEqual("public", foundResource.AccessMode);
+            Assert.IsNull(foundResource.AccessControl);
+            Assert.IsNotEmpty(foundResource.Etag);
+            Assert.Contains(m_searchTag, foundResource.Tags);
+            Assert.IsNotEmpty(foundResource.ImageMetadata);
+            Assert.AreEqual(1, foundResource.Context.Count);
+            Assert.AreEqual(0, foundResource.ImageAnalysis.FaceCount);
+            Assert.IsNotNull(foundResource.ImageAnalysis);
         }
     }
 }
