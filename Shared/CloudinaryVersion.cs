5a6132669894092fec5d01433230198f8b476408
RTLCoil <oleg@rtl.co.il>
2020-04-28 14:00:28 +0300

    Fix broken CloudinaryDotNet functionality on dotnetfiddle.net

---
+++ Shared/CloudinaryVersion.cs
@@ -0,0 +1,13 @@
+﻿namespace CloudinaryDotNet
+{
+    /// <summary>
+    /// Contains information about release version.
+    /// </summary>
+    internal static class CloudinaryVersion
+    {
+        /// <summary>
+        /// Get full version number of .NET SDK.
+        /// </summary>
+        public static string Full => "1.11.0";
+    }
+}
