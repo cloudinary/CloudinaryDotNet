56313d437193dbd5feb4125fd197f8906ecdefd9
RTLCoil <oleg@rtl.co.il>
2020-08-12 01:35:00 +0300

    Extract base class

--- Shared/Actions/MultiParams.cs
+++ Shared/Actions/MultiParams.cs
@@ -1,21 +1,19 @@
 ﻿namespace CloudinaryDotNet.Actions
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     /// <summary>
     /// Parameters of request to create a single animated GIF file from a group of images.
     /// </summary>
-    public class MultiParams : BaseParams
+    public class MultiParams : MultiAssetsParams
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MultiParams"/> class.
         /// </summary>
         /// <param name="tag">The animated GIF is created from all images with this tag.</param>
         public MultiParams(string tag)
+            : base(tag)
         {
-            Tag = tag;
         }
 
         /// <summary>
@@ -23,87 +21,8 @@
         /// </summary>
         /// <param name="urls">The animated GIF is created from all images with the urls specified.</param>
         public MultiParams(List<string> urls)
+            : base(urls)
         {
-            Urls = urls;
-        }
-
-        /// <summary>
-        /// Gets or sets the animated GIF that is created from all images with this tag.
-        /// </summary>
-        public string Tag { get; set; }
-
-        /// <summary>
-        /// Gets or sets a list of urls for all images used in the animated GIF.
-        /// </summary>
-        public List<string> Urls { get; set; } = new List<string>();
-
-        /// <summary>
-        /// Gets or sets transformation to run on all the individual images before creating the animated GIF. Optional.
-        /// </summary>
-        public Transformation Transformation { get; set; }
-
-        /// <summary>
-        /// Gets or sets an HTTP or HTTPS URL to notify your application (a webhook) when the process has completed.
-        /// </summary>
-        public string NotificationUrl { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether to perform the GIF generation in the background (asynchronously). Default: false.
-        /// </summary>
-        public bool Async { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value that can be set to 'zip' to generate a zip file containing the images instead of an animated GIF file.
-        /// Default: gif (deprecated - use the new CreateArchive method to create zip files).
-        /// </summary>
-        public string Format { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value that defines whether to return the generated file ('download').
-        /// </summary>
-        public ArchiveCallMode? Mode { get; set; }
-
-        /// <summary>
-        /// Validate object model.
-        /// </summary>
-        public override void Check()
-        {
-            var isUrlsEmpty = Urls == null || !Urls.Any();
-            if (string.IsNullOrEmpty(Tag) && isUrlsEmpty)
-            {
-                throw new ArgumentException("Either Tag or Urls must be specified");
-            }
-        }
-
-        /// <summary>
-        /// Maps object model to dictionary of parameters in cloudinary notation.
-        /// </summary>
-        /// <returns>Sorted dictionary of parameters.</returns>
-        public override SortedDictionary<string, object> ToParamsDictionary()
-        {
-            var dict = base.ToParamsDictionary();
-
-            AddParam(dict, "tag", Tag);
-            AddParam(dict, "notification_url", NotificationUrl);
-            AddParam(dict, "format", Format);
-            AddParam(dict, "async", Async);
-
-            if (Urls != null && Urls.Any())
-            {
-                AddParam(dict, "urls", Urls);
-            }
-
-            if (Transformation != null)
-            {
-                AddParam(dict, "transformation", Transformation.Generate());
-            }
-
-            if (Mode.HasValue)
-            {
-                AddParam(dict, "mode", Api.GetCloudinaryParam(Mode.Value));
-            }
-
-            return dict;
         }
     }
 }
--- Shared/Actions/SpriteParams.cs
+++ Shared/Actions/SpriteParams.cs
@@ -1,21 +1,19 @@
 ﻿namespace CloudinaryDotNet.Actions
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     /// <summary>
     /// Parameters of create sprite request.
     /// </summary>
-    public class SpriteParams : BaseParams
+    public class SpriteParams : MultiAssetsParams
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="SpriteParams"/> class.
         /// </summary>
         /// <param name="tag">The tag name assigned to images that we should merge into the sprite.</param>
         public SpriteParams(string tag)
+            : base(tag)
         {
-            Tag = tag;
         }
 
         /// <summary>
@@ -23,91 +21,8 @@
         /// </summary>
         /// <param name="urls">The sprite is created from all images with the urls specified.</param>
         public SpriteParams(List<string> urls)
+            : base(urls)
         {
-            Urls = urls;
-        }
-
-        /// <summary>
-        /// Gets or sets a value for which the sprite is created from all images with this tag.
-        /// </summary>
-        public string Tag { get; set; }
-
-        /// <summary>
-        /// Gets or sets a list of urls for all images used in the sprite.
-        /// </summary>
-        public List<string> Urls { get; set; } = new List<string>();
-
-        /// <summary>
-        /// Gets or sets a transformation to run on all the individual images before creating the sprite. Optional.
-        /// </summary>
-        public Transformation Transformation { get; set; }
-
-        /// <summary>
-        /// Gets or sets a format to convert the sprite before saving it in your Cloudinary account. Default: png. Optional.
-        /// </summary>
-        public string Format { get; set; }
-
-        /// <summary>
-        /// Gets or sets an HTTP or HTTPS URL to notify your application (a webhook) when the process has completed. Optional.
-        /// </summary>
-        public string NotificationUrl { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether to perform the sprite generation in the background (asynchronously).
-        /// Default: false.
-        /// </summary>
-        public bool Async { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value that defines whether to return the generated file ('download').
-        /// </summary>
-        public ArchiveCallMode? Mode { get; set; }
-
-        /// <summary>
-        /// Validate object model.
-        /// </summary>
-        public override void Check()
-        {
-            var isUrlsEmpty = Urls == null || !Urls.Any();
-            if (string.IsNullOrEmpty(Tag) && isUrlsEmpty)
-            {
-                throw new ArgumentException("Either Tag or Urls must be specified");
-            }
-        }
-
-        /// <summary>
-        /// Maps object model to dictionary of parameters in cloudinary notation.
-        /// </summary>
-        /// <returns>Sorted dictionary of parameters.</returns>
-        public override SortedDictionary<string, object> ToParamsDictionary()
-        {
-            var dict = base.ToParamsDictionary();
-
-            AddParam(dict, "tag", Tag);
-            AddParam(dict, "notification_url", NotificationUrl);
-            AddParam(dict, "async", Async);
-
-            if (Transformation != null)
-            {
-                AddParam(dict, "transformation", Transformation.Generate());
-            }
-
-            if (!string.IsNullOrEmpty(Format))
-            {
-                AddParam(dict, "format", Format);
-            }
-
-            if (Urls != null && Urls.Any())
-            {
-                AddParam(dict, "urls", Urls);
-            }
-
-            if (Mode.HasValue)
-            {
-                AddParam(dict, "mode", Api.GetCloudinaryParam(Mode.Value));
-            }
-
-            return dict;
         }
     }
 }
