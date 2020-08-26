59f08f0133dc92cbf450133fd00a8ac586237751
RTLCoil <oleg@rtl.co.il>
2020-08-27 00:34:14 +0300

    Add tests for pending, prefix, and sub_account_id parameters to users

--- Shared.IntegrationTests/Provisioning/AccountApi/AccountProvisioningTest.cs
+++ Shared.IntegrationTests/Provisioning/AccountApi/AccountProvisioningTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CloudinaryDotNet.Actions;
 using NUnit.Framework;
@@ -49,7 +50,7 @@ namespace CloudinaryDotNet.IntegrationTest.Provisioning.AccountApi
         {
             var newEmailAddress = $"updated+{m_timestampSuffix}@cloudinary.com";
             const string newName = "updated";
-            var updateUserParams = new UpdateUserParams(m_userId)
+            var updateUserParams = new UpdateUserParams(m_userId1)
             {
                 Email = newEmailAddress,
                 Name = newName
@@ -59,12 +60,12 @@ namespace CloudinaryDotNet.IntegrationTest.Provisioning.AccountApi
             Assert.AreEqual(newName, updateUserResult.Name);
             Assert.AreEqual(newEmailAddress, updateUserResult.Email);
 
-            var getUserResult = AccountProvisioning.User(m_userId);
-            Assert.AreEqual(m_userId, getUserResult.Id);
+            var getUserResult = AccountProvisioning.User(m_userId1);
+            Assert.AreEqual(m_userId1, getUserResult.Id);
             Assert.AreEqual(newEmailAddress, getUserResult.Email);
 
             var listUsersResult = AccountProvisioning.Users(new ListUsersParams());
-            var foundUser = listUsersResult.Users.FirstOrDefault(user => user.Id == m_userId);
+            var foundUser = listUsersResult.Users.FirstOrDefault(user => user.Id == m_userId1);
             Assert.NotNull(foundUser);
             Assert.AreEqual(newEmailAddress, foundUser.Email);
         }
@@ -74,12 +75,104 @@ namespace CloudinaryDotNet.IntegrationTest.Provisioning.AccountApi
         {
             var listUsersParams = new ListUsersParams
             {
-                UserIds = new List<string> {m_userId}
+                UserIds = new List<string> {m_userId1}
             };
 
             var result = AccountProvisioning.Users(listUsersParams);
 
             Assert.AreEqual(1, result.Users.Length);
+        }
+
+        [Test]
+        public void TestGetPendingUsers()
+        {
+            var listUsersParams = new ListUsersParams
+            {
+                Pending = true,
+                UserIds = new List<string> { m_userId1 }
+            };
+
+            var result = AccountProvisioning.Users(listUsersParams);
+
+            Assert.AreEqual(1, result.Users.Length);
+        }
+
+        [Test]
+        public void TestGetNonPendingUsers()
+        {
+            var listUsersParams = new ListUsersParams
+            {
+                Pending = false,
+                UserIds = new List<string> { m_userId1 }
+            };
+
+            var result = AccountProvisioning.Users(listUsersParams);
+
+            Assert.AreEqual(0, result.Users.Length);
+        }
+
+        [Test]
+        public void TestGetPendingAndNonPendingUsers()
+        {
+            var listUsersParams = new ListUsersParams
+            {
+                Pending = null,
+                UserIds = new List<string> { m_userId1 }
+            };
+
+            var result = AccountProvisioning.Users(listUsersParams);
+
+            Assert.AreEqual(1, result.Users.Length);
+        }
+
+        [Test]
+        public void TestGetUsersByPrefix()
+        {
+            var listUsersParams1 = new ListUsersParams
+            {
+                Pending = true,
+                Prefix = m_userName2.Substring(0, m_userName2.Length - 1)
+            };
+            var result1 = AccountProvisioning.Users(listUsersParams1);
+            Assert.AreEqual(1, result1.Users.Length);
+
+            var listUsersParams2 = new ListUsersParams
+            {
+                Pending = true,
+                Prefix = $"{m_userName2}zzz"
+            };
+            var result2 = AccountProvisioning.Users(listUsersParams2);
+            Assert.AreEqual(0, result2.Users.Length);
+        }
+
+        [Test]
+        public void TestGetUsersBySubAccountId()
+        {
+            var listUsersParams = new ListUsersParams
+            {
+                Pending = true,
+                Prefix = m_userName2,
+                SubAccountId = m_cloudId
+            };
+
+            var result = AccountProvisioning.Users(listUsersParams);
+
+            Assert.AreEqual(1, result.Users.Length);
+        }
+
+        [Test]
+        public void TestGetUsersByNonexistentSubAccountIdError()
+        {
+            var randomId = new Random().Next(100000).ToString();
+            var listUsersParams = new ListUsersParams
+            {
+                Pending = true,
+                SubAccountId = randomId
+            };
+
+            var result = AccountProvisioning.Users(listUsersParams);
+
+            Assert.True(result.Error.Message.StartsWith("Cannot find sub account"));
         }
 
         [Test]
@@ -99,13 +192,13 @@ namespace CloudinaryDotNet.IntegrationTest.Provisioning.AccountApi
         [Test]
         public void TestAddAndRemoveUserFromGroup()
         {
-            var addUserResult = AccountProvisioning.AddUserToGroup(m_groupId, m_userId);
+            var addUserResult = AccountProvisioning.AddUserToGroup(m_groupId, m_userId1);
             Assert.AreEqual(1, addUserResult.Users.Length);
 
             var listUsersResult = AccountProvisioning.UsersGroupUsers(m_groupId);
             Assert.AreEqual(1, listUsersResult.Users.Length);
 
-            var removeUserResult = AccountProvisioning.RemoveUserFromGroup(m_groupId, m_userId);
+            var removeUserResult = AccountProvisioning.RemoveUserFromGroup(m_groupId, m_userId1);
             Assert.AreEqual(0, removeUserResult.Users.Length);
         }
 
