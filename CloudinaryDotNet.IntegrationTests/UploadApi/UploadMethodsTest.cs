6ff5500cc967163c4d848e39c761584c72fa6386
Suraj <surajshinde.com>
2023-10-09 20:23:09 +0530

    fix spell error in test

--- CloudinaryDotNet.IntegrationTests/UploadApi/UploadMethodsTest.cs
+++ CloudinaryDotNet.IntegrationTests/UploadApi/UploadMethodsTest.cs
@@ -221,7 +221,7 @@ namespace CloudinaryDotNet.IntegrationTests.UploadApi
             Assert.Zero(uploadResult.Pages);
             Assert.Zero(uploadResult.Rotation);
             Assert.NotZero(uploadResult.NbFrames);
-            Assert.IsFalse(string.IsNullOrEmpty(updateResult.PlaybackUrl));
+            Assert.IsFalse(string.IsNullOrEmpty(uploadResult.PlaybackUrl));
 
             var getResource = new GetResourceParams(uploadResult.PublicId) { ResourceType = ResourceType.Video };
             var info = m_cloudinary.GetResource(getResource);
