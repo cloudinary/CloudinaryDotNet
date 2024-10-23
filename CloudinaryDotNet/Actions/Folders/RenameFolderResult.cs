eb0c10f6020e446f3449608f64638b7b92893cc1
Constantine Nathanson <35217733+const-cloudinary@users.noreply.github.com>
2024-10-23 14:10:40 +0300

    Add support for `RenameFolder` Admin API

---
+++ CloudinaryDotNet/Actions/Folders/RenameFolderResult.cs
@@ -0,0 +1,23 @@
+﻿namespace CloudinaryDotNet.Actions
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Parsed result of folders listing.
+    /// </summary>
+    [DataContract]
+    public class RenameFolderResult : BaseResult
+    {
+        /// <summary>
+        /// Gets or sets original folder.
+        /// </summary>
+        [DataMember(Name = "from")]
+        public Folder From { get; set; }
+
+        /// <summary>
+        /// Gets or sets new folder.
+        /// </summary>
+        [DataMember(Name = "to")]
+        public Folder To { get; set; }
+    }
+}
